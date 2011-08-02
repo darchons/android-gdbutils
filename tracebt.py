69c9776cb6d1079aed8beca5ee910f32901083bc
Jim Chen <jimnchen@gmail.com>
2011-08-02 00:45:37 -0700

    tracebt: 'class'ification

--- tracebt.py
+++ tracebt.py
@@ -40,32 +40,41 @@ from logging import debug, info, warning, error, critical, exception
 
 re_inst = re.compile(r'.+(0x[\da-f]+):\s+([\d\w_\.]+)\s+([^;]*);?')
 
-def tracebt ():
+class Frame:
 
-    def doReport (pc, sp, is_thumb):
-        gdb.execute('frame ' + hex(sp) + ' ' + hex(pc), False, False)
+    def __init__(self, pc, sp, is_thumb):
+        self.pc = pc
+        self.sp = sp
+        self.is_thumb = is_thumb
 
-    def doTrace (pc, sp, is_thumb):
+    def print(self)
+        gdb.execute('frame ' + hex(self.sp) + ' ' + hex(self.pc), False, False)
+
+    def _unwind(self):
         BLOCK_SIZE = 0x80
+        pc = self.pc
+        sp = self.sp
+        is_thumb = self.is_thumb
         branchList = []
+        condBranchCount = 0
 
-        def doTraceBlock (pc, sp, is_thumb):
+        while True:
             debug('start block @ %x : %x', pc, sp)
-            condBranchSkipped = 0
 
             # adjust pc according to ARM/THUMB mode
             pc = pc | 1 if is_thumb else pc & (~3)
             # disassemble a block of instructions at pc
             insts = gdb.execute('disassemble ' + hex(pc) +
-                                ',' + hex(pc + BLOCK_SIZE),
+                                    ', ' + hex(pc + BLOCK_SIZE),
                                 False, True).lower().splitlines()
+
             for inst in insts[: -2]: # avoid misdisassembly at the end
                 match = re_inst.match(inst)
                 if not match:
                     continue
+
                 # update pc
                 pc = int(match.group(1).strip(), 0)
-
                 # get mnemonic and strip width qualifier
                 mnemonic = match.group(2).strip()
                 mnemonic = mnemonic[:-2] \
@@ -76,29 +85,29 @@ def tracebt ():
                 args = args[: args.find('<')].strip()
 
                 # trace certain instructions
-                def doTraceBranch (pc, sp, is_thumb, mnemonic, args):
+                def traceBranch():
+                    info('b addr @ %x : %x', pc, sp)
+                    new_pc = int(args, 0)
+                    new_is_thumb = not is_thumb if mnemonic.startswith('bx') \
+                                    else is_thumb
+                    branchList.append((new_pc, condBranchCount))
+                    return (new_pc, new_is_thumb)
+
+                if mnemonic == 'b' or mnemonic == 'bx' or \
+                    mnemonic == 'bal' or mnemonic == 'bxal':
                     if args == 'lr':
                         # FIXME lr might not be valid
                         warning('bx lr @ %x : %x', pc, sp)
                         pc = int(gdb.parse_and_eval('(unsigned)$lr'))
-                        doReport(pc, sp, (pc & 1) != 0)
-                        return (pc, sp, (pc & 1) != 0)
-                    info('b addr @ %x : %x', pc, sp)
-                    pc = int(args, 0)
-                    branchList.append((pc, condBranchSkipped))
-                    return (pc, sp, is_thumb
-                        if mnemonic.startswith('bx') else not is_thumb)
-
-                if mnemonic == 'b' or mnemonic == 'bx' or \
-                    mnemonic == 'bal' or mnemonic == 'bxal':
-                    print 'skipped condB: ' + str(condBranchSkipped)
-                    condBranchSkipped = 0
-                    return doTraceBranch(pc, sp, is_thumb, mnemonic, args)
+                        return Frame(pc, sp, (pc & 1) != 0)
+                    (pc, is_thumb) = traceBranch()
+                    condBranchCount = 0
+                    break
 
                 elif mnemonic.startswith('b') and mnemonic.lstrip('bx') in \
                     ['eq', 'ne', 'cs', 'cc', 'hs', 'lo', 'mi', 'pl',
                      'vs', 'vc', 'hi', 'ls', 'ge', 'lt', 'gt', 'le']:
-                    condBranchSkipped += 1
+                    condBranchCount += 1
 
                 elif mnemonic == 'vpush':
                     sp -= 8 * len(args[args.find('{') :].split(','))
@@ -119,7 +128,7 @@ def tracebt ():
                         pc = int(gdb.parse_and_eval('*(unsigned*)' +
                                 hex(sp - 4 * len(args.split(',')))))
                         doReport(pc, sp, (pc & 1) != 0)
-                        return (pc, sp, (pc & 1) != 0)
+                        return Frame(pc, sp, (pc & 1) != 0)
 
                 elif mnemonic == 'add' and args.startswith('sp'):
                     assert args.split(',')[1].find('r') < 0, \
@@ -131,24 +140,27 @@ def tracebt ():
                             'SUB with ' + args + ' (pc = ' + hex(pc) + ')'
                     sp -= int(args[args.find('#') + 1 :], 0)
 
-            debug('end blocki @ %x : %x', pc, sp)
-            return (pc, sp, is_thumb)
+            debug('end block @ %x : %x', pc, sp)
 
+    def unwind(self)
         # don't let value of cpsr affect our results
         saved_cpsr = int(gdb.parse_and_eval('$cpsr'))
-        gdb.execute('set $cpsr=' +
-                    hex(saved_cpsr & 0x03df), False, True)
-
+        gdb.execute('set $cpsr=' + hex(saved_cpsr & 0x03df))
         try:
-            for i in range(0x10):
-                (pc, sp, is_thumb) = doTraceBlock(pc, sp, is_thumb)
+            return self._unwind()
+        except:
+            exception('Frame.unwind')
         finally:
-            gdb.execute('set $cpsr=' + hex(saved_cpsr), False, True)
+            gdb.execute('set $cpsr=' + hex(saved_cpsr))
 
-    pc = int(gdb.parse_and_eval('(unsigned)$pc'))
-    sp = int(gdb.parse_and_eval('(unsigned)$sp'))
-    is_thumb = (gdb.parse_and_eval('$cpsr') & 0x20) != 0
-    doTrace(pc, sp, is_thumb)
+pc = int(gdb.parse_and_eval('(unsigned)$pc'))
+sp = int(gdb.parse_and_eval('(unsigned)$sp'))
+is_thumb = (gdb.parse_and_eval('$cpsr') & 0x20) != 0
 
-tracebt()
+f = Frame(pc, sp, is_thumb)
+f.print()
+
+for i in range(5)
+    f = f.unwind()
+    f.print()
 
