58c756c911e5d5893446d719a79a8709adeafe77
Jim Chen <jimnchen@gmail.com>
2011-09-09 19:57:57 -0700

    adblog: parsing/coloring/filtering support

--- python/adblog.py
+++ python/adblog.py
@@ -35,33 +35,113 @@
 #
 # ***** END LICENSE BLOCK *****
 
-import gdb, adb, threading, sys
+import gdb, adb, feninit, threading, sys, os, cStringIO, collections
+
+ADBLogEntry = collections.namedtuple('ADBLogEntry',
+        ['date', 'time', 'pid', 'tid', 'priority', 'tag', 'text']);
+
+def default_filter(entry):
+    global log_width, log_color
+    if not hasattr(feninit.default, 'pid') or \
+            entry.pid == feninit.default.pid:
+        log_color = log_color + 1 if log_color <= 5 else 2
+        text = entry.text
+        if log_width > 8 and len(text) + 5 > log_width:
+            text = text[0: log_width - 8] + '...'
+        return 'adb| \x1B[3' + str(log_color) + 'm' + text + '\x1B[39m\n'
+    return None
+
+log_color = 1
+log_filter = default_filter;
 
 class ADBLog(threading.Thread):
+
+    def _parseLog(self, logFile):
+        line = ''
+        while not line.startswith('['):
+            line = logFile.next()
+        # line == '[ DAY TIME PID:TID PRIO/TAG ]'
+        items = line.strip('[] \t\r\n').split()
+        text = []
+        line = logFile.next().strip()
+        while line:
+            text.append(line)
+            line = logFile.next().strip()
+        pidtid = items[2].partition(':')
+        priotag = items[3].partition('/')
+        return ADBLogEntry(items[0], items[1],
+                pidtid[0], pidtid[2],
+                priotag[0], priotag[2], '\n'.join(text));
+
     def __init__(self):
         super(ADBLog, self).__init__()
-        self.logcat = adb.call(['logcat', '-v', 'thread'], async=True)
+        logcatArgs = ['-v', 'long',
+                'Gecko:V', 'GeckoChildLoad:V', 'GeckoFonts:V',
+                'GeckoMapFile:V', 'GeckoLibLoad:V', 'fennec:V', '*:S']
+
+        logCount = 0
+        dump = cStringIO.StringIO(adb.call(['logcat', '-d'] + logcatArgs))
+        try:
+            while True: # parse until the end of log
+                self._parseLog(dump)
+                logCount += 1
+        except StopIteration:
+            pass
+
+        def adblogPreExec():
+            os.setpgrp()
+        self.logcat = adb.call(['logcat'] + logcatArgs,
+                async=True, preexec_fn=adblogPreExec)
+        while logCount:
+            self._parseLog(self.logcat.stdout)
+            logCount -= 1
+
+        self.running = False
 
     def run(self):
-        while self.logcat.poll() == None:
-            line = self.logcat.stdout.readline()
-            if line:
-                sys.__stderr__.write(line)
-
-    def stop_handler(self, event):
         try:
-            self.logcat.terminate()
-        except OSError:
-            pass # process might be already terminated
-        gdb.events.stop.disconnect(self.stop_handler)
+            global log_filter
+            while self.logcat.poll() == None:
+                entry = self._parseLog(self.logcat.stdout)
+                if not self.running:
+                    continue
+                log = log_filter(entry)
+                if not log:
+                    continue
+                sys.__stderr__.write(log)
+        except StopIteration:
+            pass
+
+    def terminate(self):
+        self.logcat.terminate();
 
 def cont_handler(event):
     if not isinstance(event, gdb.ContinueEvent):
         return
-    adb.chooseDevice()
-    adblog = ADBLog()
-    gdb.events.stop.connect(adblog.stop_handler)
-    adblog.start()
+    global adblog, log_width
+    if not adblog:
+        adb.chooseDevice()
+        adblog = ADBLog()
+        adblog.start()
+    log_width = int(gdb.parameter('width'))
+    adblog.running = True
 
+def stop_handler(event=None):
+    global adblog
+    if not adblog:
+        return
+    adblog.running = False
+
+def exit_handler(event=None):
+    global adblog
+    if not adblog:
+        return
+    adblog.running = False
+    adblog.terminate()
+    adblog = None
+
+adblog = None
 gdb.events.cont.connect(cont_handler)
+gdb.events.stop.connect(stop_handler)
+gdb.events.exited.connect(exit_handler)
 
